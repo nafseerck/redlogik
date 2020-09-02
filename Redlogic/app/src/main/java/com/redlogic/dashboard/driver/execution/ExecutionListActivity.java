e3429310f6a2fcac7d9d18e61b475e5e7d2fdd81
Jithin v <jithin.v@cgtindia.in>
2020-09-02 17:09:12 +0530

    check box tick orderely

--- Redlogic/app/src/main/java/com/redlogic/dashboard/driver/execution/ExecutionListActivity.java
+++ Redlogic/app/src/main/java/com/redlogic/dashboard/driver/execution/ExecutionListActivity.java
@@ -217,6 +217,7 @@ public class ExecutionListActivity extends BaseLoaderActivity {
             public void onResponseSuccess(String responseBodyString) {
                 hideDialog();
                 updateCompleteJobBtnLastExecution();
+                getNextCheckboxToComplete();
             }
 
             @Override
